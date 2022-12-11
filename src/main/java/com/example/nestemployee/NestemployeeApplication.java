cd3bad9bf67a13d1d3ced5e084a3a017aa03af17
Ajay957 <ajay74.aa82@gmail.com>
2022-12-12 03:25:21 +0530

    first commit

---
+++ src/main/java/com/example/nestemployee/NestemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nestemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestemployeeApplication.class, args);
+	}
+
+}
